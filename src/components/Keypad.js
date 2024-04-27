ae679d0bfd615c7df70539a3969ec70c41e46e0d
+ <+>
2024-04-27 19:32:43 +0300

    Done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,15 @@
 // Code Keypad Component Here
 
+import React from "react";
+
 function Keypad (){
+    function change(){
+        console.log('Entering password...')
+    }
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={change} name="password" placeholder="Enter the number"/>
+        </div>
     )
 }
 
